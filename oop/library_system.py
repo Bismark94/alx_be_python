ec159f8ae504c472e197ee84de61d9ec92b14b72
Bismark Agbemafle <agbemaflebismark@gmail.com>
2025-06-22 20:51:58 +0000

    Mastering Inheritance and Composition in Python

--- oop/library_system.py
+++ oop/library_system.py
@@ -12,7 +12,7 @@ class Book:
         return f"Book({self.title!r}, {self.author!r})"
 
 
-class Ebook(Book):
+class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
